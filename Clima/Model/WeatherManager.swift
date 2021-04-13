e6b34c9105dfad39ab3725843f3d4df0d13e8e44
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-12 20:47:04 -0700

    Add JSONDecoder to decode JSON data from API. Use trailing functions as the "callback" function when API calls completed.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -11,10 +11,11 @@ import Foundation
 struct WeatherManager {
     let baseURL = "https://api.openweathermap.org/data/2.5/weather?"
     let cityName = "London"
-    let weatherURL = "&appid=22e016f4c12443ec4a5383e7ae043dfd&units=metric"
+    let apiKey = "&appid=22e016f4c12443ec4a5383e7ae043dfd"
+    let unit = "&units=metric"
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(baseURL)q=\(cityName)&appid=22e016f4c12443ec4a5383e7ae043dfd&units=metric"
+        let urlString = "\(baseURL)q=\(cityName)\(unit)\(apiKey)"
         print("\(urlString)")
         performRequest(urlString: urlString)
     }
@@ -23,22 +24,29 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
             
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             task.resume()
-            
         }
-        
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+        } catch {
+            print(error)
         }
         
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
     }
 }
---
+++ Clima/Model/weatherData.swift
@@ -0,0 +1,22 @@
+//
+//  weatherData.swift
+//  Clima
+//
+//  Created by Zimo Zhao on 4/12/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String    
+}
